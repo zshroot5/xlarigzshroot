56b80728bc74a64c5faf1f72971ffc60d2b48062
zshroot5 <59542285+zshroot5@users.noreply.github.com>
2021-03-17 19:32:55 -0300

    Add files via upload

---
+++ xlarig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install git build-essential cmake automake libtool autoconf libuv1-dev
+git clone https://github.com/scala-network/xlarig
+mkdir xlarig/build && cd xlarig/build
+cmake ..
+make -j$(nproc)
+./xlarig --donate-level 0 -o scala.herominers.com:10130 -u Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1MRCr8v6Us -p x -a panthera -k --cpu-affinity 3 --cpu-priority 5 -t 2
